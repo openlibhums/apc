cfa7cd8a362f964da9e51e3f06dc27103865d93f
Andy Byers <andy.byers@openlibhums.org>
2020-01-22 10:15:33 +0000

    Checks that the APC plugin is installed.

--- logic.py
+++ logic.py
@@ -2,7 +2,8 @@ from django.shortcuts import get_object_or_404
 from django.contrib import messages
 
 from submission import models as submission_models
-from plugins.apc import forms, models
+from plugins.apc import forms, models, plugin_settings
+from utils import setting_handler
 
 
 def handle_set_apc(request, form):
@@ -28,17 +29,27 @@ def get_waiver_status_from_post(post):
 def set_apc(**kwargs):
     request = kwargs.get('request', None)
     article = kwargs.get('article', None)
+    plugin = plugin_settings.get_self()
 
     if request and article:
-        try:
-            section_apc = models.SectionAPC.objects.get(section=article.section)
-        except models.SectionAPC.DoesNotExist:
-            messages.add_message(request, messages.WARNING, 'APC Management is enabled but this section has no APC.')
-            return
 
-        models.ArticleAPC.objects.create(
-            article=article,
-            section_apc=section_apc,
-            value=section_apc.value,
-            currency=section_apc.currency,
+        enable_apcs = setting_handler.get_plugin_setting(
+            plugin,
+            'enable_apcs',
+            request.journal,
         )
+
+        if enable_apcs.processed_value:
+
+            try:
+                section_apc = models.SectionAPC.objects.get(section=article.section)
+            except models.SectionAPC.DoesNotExist:
+                messages.add_message(request, messages.WARNING, 'APC Management is enabled but this section has no APC.')
+                return
+
+            models.ArticleAPC.objects.create(
+                article=article,
+                section_apc=section_apc,
+                value=section_apc.value,
+                currency=section_apc.currency,
+            )
