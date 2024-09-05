8cd9655006f7897949009e3665cc725dfeaf5e9b
Andy Byers <a.byers@bbk.ac.uk>
2024-09-05 16:25:38 +0100

    Updates plugin for 1.7 release.

--- urls.py
+++ urls.py
@@ -1,41 +1,41 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from plugins.apc import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='apc_index'),
-    url(r'^waiver/(?P<application_id>\d+)/$',
+    re_path(r'^$', views.index, name='apc_index'),
+    re_path(r'^waiver/(?P<application_id>\d+)/$',
         views.waiver_application,
         name='apc_waiver_application',
     ),
-    url(r'^waiver/new/(?P<article_id>\d+)/$',
+    re_path(r'^waiver/new/(?P<article_id>\d+)/$',
         views.make_waiver_application, 
         name='apc_make_waiver_application',
     ),
-    url(r'^settings/$', views.settings, name='apc_settings'),
+    re_path(r'^settings/$', views.settings, name='apc_settings'),
 
-    url(r'^(?P<apc_id>\d+)/action/(?P<action>paid|unpaid|new|invoiced)/$',
+    re_path(r'^(?P<apc_id>\d+)/action/(?P<action>paid|unpaid|new|invoiced)/$',
         views.apc_action,
         name='apc_action',
     ),
-    url(r'^staff/$', views.billing_staff, name='apc_billing_staff'),
+    re_path(r'^staff/$', views.billing_staff, name='apc_billing_staff'),
     
-    url(
+    re_path(
         r'^staff/new/$', 
         views.manage_billing_staff, 
         name='apc_new_billing_staff',
     ),
-    url(
+    re_path(
         r'^staff/(?P<billing_staffer_id>\d+)/$', 
         views.manage_billing_staff, 
         name='apc_manage_billing_staff',
     ),
-    url(
+    re_path(
         r'^add_article/$',
         views.add_article,
         name='apc_add_article',
     ),
-    url(r'^discount/(?P<apc_id>\d+)/$',
+    re_path(r'^discount/(?P<apc_id>\d+)/$',
         views.discount_apc,
         name='discount_apc',
     ),
